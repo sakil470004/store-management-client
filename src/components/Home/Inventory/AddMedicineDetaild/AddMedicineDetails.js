c31cc76cb64c0f4bf95484d3bb6cdc28ce1a8a13
Mynul Islam <sk470004@gmail.com>
2022-01-28 00:45:42 +0600

    finishing allmost full ui

---
+++ src/components/Home/Inventory/AddMedicineDetaild/AddMedicineDetails.js
@@ -0,0 +1,112 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { TextField } from '@mui/material';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    borderRadius: '50px',
+    boxShadow: 24,
+    p: 4,
+
+
+};
+
+export default function AddMedicineDetails({ open, setOpen }) {
+    const handleClose = () => setOpen(false);
+    const [order, setOrder] = React.useState({})
+    const form = React.useRef(null)
+
+    const handleOnBlur = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newOrderData = { ...order };
+        newOrderData[field] = value;
+        setOrder(newOrderData)
+
+    }
+    const handleCreateOrder = (e) => {
+        // console.log(product)
+        // send data to the server
+
+
+        e.preventDefault()
+    }
+
+    return (
+        <div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                   
+                            <h2>Add Medicine Details</h2>
+                            <form
+                                ref={form}
+                                onSubmit={handleCreateOrder}>
+
+                                <TextField
+                                    required
+                                    sx={{ width: '90%', m: 2 }}
+                                    label="Medicine Name"
+                                    variant="standard"
+                                    name='medicineName'
+                                    onBlur={handleOnBlur} />
+
+                                <TextField
+                                    required
+                                    sx={{ width: '90%', m: 2 }}
+                                    label="Manufacturer"
+                                    variant="standard"
+                                    name='manufacturer'
+                                    onBlur={handleOnBlur} />
+                                <TextField
+                                    required
+                                    sx={{ width: '40%', m: 2 }}
+                                    label="Price"
+                                    variant="standard"
+                                    name='price'
+                                    type='number'
+
+                                    onBlur={handleOnBlur} />
+
+                                <TextField
+                                    required
+                                    sx={{ width: '40%', m: 2 }}
+                                    label="Stock"
+                                    variant="standard"
+                                    name='stock'
+                                    type='number'
+                                    onBlur={handleOnBlur} />
+
+                                <TextField
+                                    required
+                                    sx={{ width: '40%', m: 2 }}
+                                    label="Discount"
+                                    variant="standard"
+                                    name='discount'
+                                    type='number'
+                                    onBlur={handleOnBlur} />
+
+
+                                <button
+
+                                    style={{ marginTop: 20, width: '90%', backgroundColor: '#1976D2', color: 'white', padding: '10px', borderRadius: '15px', cursor: 'pointer' }} type='submit'>Add To The Inventory</button>
+
+                            </form>
+                        </Box>
+            </Modal>
+        </div>
+    );
+}
--- src/components/Home/Inventory/Inventory.js
+++ src/components/Home/Inventory/Inventory.js
@@ -2,13 +2,26 @@ import React, { useState } from 'react';
 import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CreateIcon from '@mui/icons-material/Create';
+import AddMedicineDetails from './AddMedicineDetaild/AddMedicineDetails';
+import AddIcon from '@mui/icons-material/Add';
 
 
 
 export default function Inventory() {
     const [carts, setCarts] = useState([]);
+    const handleOpen = () => setOpen(true);
+    const [open, setOpen] = React.useState(false);
+
+
     return <div>
         <h2>Inventory</h2>
+        <div style={{display:'flex',justifyContent:'left',marginBottom:15}}>
+            <Button startIcon={<AddIcon/>} onClick={handleOpen}>Add New Medicine</Button>
+        </div>
+        <AddMedicineDetails
+            setOpen={setOpen}
+            open={open}
+        />
         <TableContainer component={Paper}>
             <Table sx={{}} aria-label="Appointments table">
                 <TableHead>
@@ -18,7 +31,7 @@ export default function Inventory() {
                         <TableCell>Price</TableCell>
                         <TableCell align="right">Stock</TableCell>
                         <TableCell align="right">Discount(%)</TableCell>
-                        <TableCell align="right">Action</TableCell>
+                        <TableCell align="center">Action</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -39,8 +52,8 @@ export default function Inventory() {
                         <TableCell align="right">stock</TableCell>
                         <TableCell align="right">discount</TableCell>
                         <TableCell align="right">
-                        <Button><CreateIcon /></Button>
-                        <Button style={{ color: 'red' }}><DeleteIcon /></Button></TableCell>
+                            <Button><CreateIcon /></Button>
+                            <Button style={{ color: 'red' }}><DeleteIcon /></Button></TableCell>
 
                     </TableRow>
                     {/* ))} */}
